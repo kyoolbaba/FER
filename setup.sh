10f33ad69b46162123e93d0704b70fac7eeb3abc
milanjp <kyoolbaba@gmail.com>
2022-05-30 16:53:09 +0530

    m3

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
+echo "\
+[genera]\n\
+email = \"dorrego.cabrera@gmail.com\"\n\
+"> ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
+enableXsrfProtection = false\n\
+enableCORS = false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
